c836786d43f54a3f90e8107c4418b8c8a2ab1540
kauaburtuli <kaua.burtuli@escola.pr.gov.br>
2026-08-24 22:54:30 -0300

    separação do código

---
+++ projeto-individual.js
@@ -0,0 +1,15 @@
+function abrirImagem(src) {
+
+                document.getElementById("janelaImagem").style.display = "flex";
+
+                document.getElementById("imagemGrande").src = src;
+
+            }
+
+            function fecharImagem() {
+
+                document.getElementById("janelaImagem").style.display = "none";
+
+            }
+
+            
